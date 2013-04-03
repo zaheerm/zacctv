7888c271e8d690ece78c308ad12356cbab31fb9f
Zaheer Abbas Merali <zaheermerali@gmail.com>
2013-04-03 23:02:59 +0100

    Made cleaner walking through the directories

--- remove_all_mp4_files.py
+++ remove_all_mp4_files.py
@@ -7,16 +7,20 @@ import time
 
 BASE_DIR = "/tmp/cctv"
 
+def older_than(filename, minutes=300):
+    if time.time() - os.path.getmtime(filename) > minutes:
+        return True
+    else:
+        return False
+
 def main():
-    directories = os.listdir(BASE_DIR)
-    for directory in directories:
-        mp4_files = os.listdir(os.path.join(BASE_DIR, directory))
+    for containing_dir, directories, files in os.walk(BASE_DIR):
+        mp4_files = [mp4_file for mp4_file in files if mp4_file.endswith(".mp4")]
         for mp4_file in mp4_files:
-            if mp4_file.endswith(".mp4"):
-                full_mp4_file = os.path.join(BASE_DIR, directory, mp4_file)
-                file_mtime = os.path.getmtime(full_mp4_file)
-                if time.time() - file_mtime > 60 * 5:
-                    os.unlink(full_mp4_file)
+            full_mp4_file = os.path.join(containing_dir, mp4_file)
+            file_mtime = os.path.getmtime(full_mp4_file)
+            if older_than(full_mp4_file, minutes=60*5):
+                os.unlink(full_mp4_file)
 
 if __name__ == '__main__':
     main()
